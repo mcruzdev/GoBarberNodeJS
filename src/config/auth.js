2d19dc3c50447db35e8e41c074401aeadb0c799e
Matheus Cruz <bromatheuscruz@gmail.com>
2019-11-20 03:14:23 -0300

    Adicionando token jwt

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd3sa8316ed8eaed7f65dbc7836bcrb06e0te5od',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,11 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res, next) => {
-  const user = await User.create({
-    name: 'Matheus Cruz',
-    email: 'matheuscruz.dev@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
