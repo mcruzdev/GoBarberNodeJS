d47d7dc80352884fd4736c27721855a0f46ffb92
Matheus Cruz <bromatheuscruz@gmail.com>
2019-11-20 02:32:20 -0300

    Carregando models e testando insercao

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res, next) => res.json('Hello world'));
+routes.get('/', async (req, res, next) => {
+  const user = await User.create({
+    name: 'Matheus Cruz',
+    email: 'matheuscruz.dev@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
