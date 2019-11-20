f16c0fec9a1692387e63fb085276bef65875c63c
Matheus Cruz <bromatheuscruz@gmail.com>
2019-11-20 03:46:01 -0300

    update no user utilizando middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,8 +13,46 @@ class UserController {
         error: 'User already exists',
       });
     }
-    const user = await User.create(req.body);
-    return res.json(user);
+
+    const {
+      id, name, email, provider,
+    } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.user_id);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const {
+      id, name, provider,
+    } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      provider,
+      email,
+    });
   }
 }
 
