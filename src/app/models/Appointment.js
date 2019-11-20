a059de556ab7c7f44acb3a9fa9024ef6867b1f17
Matheus Cruz <matheuscruz.dev@gmail.com>
2019-11-20 16:24:40 -0300

    Criando agendamentos

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      },
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id' });
+  }
+}
+
+export default Appointment;
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,5 +22,7 @@ routes.post('/sessions', SessionController.store);
 routes.get('/providers', authMiddleware, ProviderController.index);
 
 routes.post('/files', authMiddleware, upload.single('file'), FileController.store);
+
+routes.post('/appointments', authMiddleware, AppointmentController.store);
 
 export default routes;
